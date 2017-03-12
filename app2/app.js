6181915cb05c5b60ab60f6b56d251e9d5e3007a5
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2017-03-12 14:17:33 +0530

    App 2 commit

---
+++ app2/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("myApp2", []);
+
+app.controller("controller1", function() {
+	this.name="ABHINAV";
+});
+
+app.controller("controller2", function() {
+	this.name="GUPTA";
+});
